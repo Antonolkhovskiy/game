7d394f842e1598e345f460ebe4a2c9ccc9cea167
Dovlet Amanov <turkmenium@gmail.com>
2017-12-25 14:00:21 +0300

    sdfds

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports ={
 	'port': process.env.PORT || 3000,
-	'MONGODB_URI': 'mongodb://dovik:seproject@ds127126.mlab.com:27126/game' || 'mongodb://localhost/game'
+	'MONGODB_URI': 'mongodb://dovik:seproject@ds127126.mlab.com:27126/game'
 };
